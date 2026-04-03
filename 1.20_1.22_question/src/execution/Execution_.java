db8f247a4055cf383914ad8da87b73f45df81b53
lk78y <lk78y@DESKTOP-MEIBI55.lan>
2026-04-03 19:39:46 +0900

    java課題1.20_1.22修正

---
+++ 1.20_1.22_question/src/execution/Execution_.java
@@ -0,0 +1,13 @@
+package execution;
+
+import process.Process_;
+
+public class Execution_ {
+    public static void main(String[] args) {
+        // 処理用クラスの呼び出し
+    	Process_ logic = new Process_();
+        
+        // メソッドの実行
+        logic.displayInformation();
+    }
+}
